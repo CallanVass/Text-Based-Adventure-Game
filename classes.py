9fd74eb2f8af66ee1f2ab0f6219c7db50994f1c3
Callan <callan.v@fake.com>
2023-10-20 07:37:19 +1100

    Added basic notebook functions, started character and inventory

--- classes.py
+++ classes.py
@@ -2,14 +2,33 @@ class Character:
     def __init__(self, name, health, bloodlust):
         self.name = name
         self.health = health
-        self.bloodlust = bloodlust
-        self.inv = Inventory([])
+        self.bloodlust = f"{bloodlust}/100"
+        self.inv = Inventory()
+    
+    def check_inventory(self):
+        print(self.inv)
 
 class Inventory:
-    pass
+    def __init__(self):
+        self.items = []
+
+    def add_items(self, name):
+        items = Inventory
+        self.items.append(name)
+    
+
 
 class Notebook:
-    pass
+    def read_notebook(self):
+        with open("notebook.txt", "r") as file:
+            file_writings = file.read()
+            print("You look at your arm.")
+            print(file_writings)
+
+    def write_notebook(self):
+        with open("notebook.txt", "a") as file:
+            user_input = input("What would you like to carve? ")
+            file.write(f"{user_input}\n")
 
 class Options:
     pass
@@ -23,5 +42,7 @@ class Main:
     pass
 
 class Room:
-    pass
+    def room_inventory(self):
+        self.inv = Inventory([])
+
 
---
+++ main.py
@@ -0,0 +1,14 @@
+from classes import *
+
+
+
+# #NOTEBOOK FUNCTION
+# notebook = Notebook()
+
+# notebook.read_notebook()
+# notebook.write_notebook()
+
+#Character Creation
+main_character = Character("Argus", 100, 0)
+
+main_character.check_inventory()
