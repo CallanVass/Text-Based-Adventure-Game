fccf82112fed73fb806d7bd9705ab4c387140a4f
Callan <callan.v@fake.com>
2023-10-19 11:04:50 +1100

    Initial commit

---
+++ classes.py
@@ -0,0 +1,24 @@
+class Character:
+    def __init__(self, name, health, bloodlust):
+        self.name = name
+        self.health = health
+        self.bloodlust = bloodlust
+        self.inv = Inventory([])
+
+class Inventory:
+    pass
+
+class Options:
+    pass
+
+class Chance:
+
+    def chance_of_success(percentage):
+        pass
+
+class Main:
+    pass
+
+class Room:
+    pass
+
