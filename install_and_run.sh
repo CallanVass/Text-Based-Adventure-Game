254f46d4143e540b1357cbe216ab14239c9d6536
Callan <callan.v@fake.com>
2023-10-27 07:46:30 +1100

    Testing out shell script.

---
+++ install_and_run.sh
@@ -0,0 +1,35 @@
+# !/bin/bash
+
+# Python Installation
+if ! command -v python3 &>/dev/null; then
+    echo "Python is not installed. Installing..."
+    sudo apt update
+    sudo apt install python3
+else
+    echo "Python is already installed."
+fi
+
+# Pip Installation
+if ! command -v pip3 &>/dev/null; then
+    echo "pip is not installed. Installing..."
+    sudo apt install python3-pip
+else
+    echo "pip is already installed."
+fi
+
+#Requirements.txt Installation
+if [ -f requirements.txt ]; then
+    echo "Installing Python packages from requirements.txt..."
+    pip3 install -r requirements.txt
+else
+    echo "requirements.txt file not found. Skipping package installation."
+fi
+
+# Running Main
+if [ -f main.py ]; then
+    echo "Running main.py..."
+    python3 main.py
+else
+    echo "main.py file not found. Skipping execution."
+fi
+
